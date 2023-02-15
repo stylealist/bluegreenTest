d400fc4d4c2a609d3b54b4a39a99efe4b1e50d12
stylealist <52662928+stylealist@users.noreply.github.com>
2023-02-15 13:44:22 +0900

    update TestController.java
    리턴값 수정

--- src/main/java/com/example/bluegreentest/controller/TestController.java
+++ src/main/java/com/example/bluegreentest/controller/TestController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
 
-    public void test01() {
-
+    public String test01() {
+        return "Test";
     }
 
 }
