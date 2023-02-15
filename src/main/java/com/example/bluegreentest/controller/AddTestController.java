d8e6e0aabef9fd173f32df63eec6ecb37ccbb5b8
workspace <stylealist@gmail.com>
2023-02-15 12:38:07 +0900

    feat: 신규 TestController 추가
    API 테스트를 위한 TestController 추가

---
+++ src/main/java/com/example/bluegreentest/controller/AddTestController.java
@@ -0,0 +1,4 @@
+package com.example.bluegreentest.controller;
+
+public class AddTestController {
+}
---
+++ src/main/java/com/example/bluegreentest/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.bluegreentest.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+    public void test01() {
+
+    }
+
+}
